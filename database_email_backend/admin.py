a5192da2d9701975a037d79fab77427cace34e07
jpham <mahp.jp@gmail.com>
2019-04-15 16:53:32 +0800

    Django 2.0

--- database_email_backend/admin.py
+++ database_email_backend/admin.py
@@ -3,9 +3,9 @@ from django.http import HttpResponseRedirect
 from django.contrib import admin
 from django import forms
 from django.http import HttpResponse
-from django.core.urlresolvers import reverse
 from django.core.mail import message
 from django.db.models import Count
+from django.urls import reverse
 from django.utils.functional import update_wrapper
 from django.utils.translation import ugettext as _
 from django.template.defaultfilters import linebreaks_filter
